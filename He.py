0372a818fd9cbacd1499bafb96088619901895d9
pushparaj26 <62799614+pushparaj26@users.noreply.github.com>
2023-01-23 15:03:06 +0530

    Add files via upload

---
+++ He.py
@@ -0,0 +1,25 @@
+import CGM_DASH
+import streamlit as st
+#import movement_dash
+
+
+
+    
+PAGES = {
+    "Metabolic Index": CGM_DASH}
+    #"Cardiovascular Index":movement_dash
+#st.snow()
+st.sidebar.image("HE_Word.png", width=300)
+st.sidebar.title('Navigation')
+
+selection = st.sidebar.radio("Go to Index", list(PAGES.keys()))
+page = PAGES[selection]
+page.app()
+
+
+
+
+
+
+    
+set_background("HE_BACK.png")
